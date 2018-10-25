205953604c02fa8fc4827f5f61bc5d1165240e4e
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-10-25 15:10:22 +0200

    Add notifyReducer to store

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import 'firebase/firestore';
 import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 // Reducers
-// @todo
+import notifyReducer from './reducers/notifyReducer';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCT0GKrMBSjRoqOeY-Lt8sK88kvm5wQjpw',
@@ -37,7 +37,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  notify: notifyReducer
 });
 
 // Create initial state
